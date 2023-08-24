54ffda7599abf3190266e5fc5239fb11bfc02ce0
Artur <artur.h.0427@gmail.com>
2023-08-24 16:34:40 +0200

    Setup react native router system

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+/* Fonts */
+import { useFonts } from "expo-font";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+/* Scene imports */
+import Camera from "./scenes/camera/Camera";
+import Preview from "./scenes/preview/Preview";
+import Result from "./scenes/result/Result";
+
+import { enableScreens } from "react-native-screens";
+import Setup from "./scenes/setup/Name";
+import HowOften from "./scenes/setup/HowOften";
+enableScreens();
+
+/* Scenes */
+const Stack = createStackNavigator();
+
+const App = () => {
+	let commonConfig = { headerShown: false, animationEnabled: false };
+
+	const [fontsLoaded] = useFonts({
+		"inter-extrabold": require("./assets/fonts/Inter-ExtraBold.ttf"),
+		"inter-black": require("./assets/fonts/Inter-Black.ttf"),
+		"poppins-black": require("./assets/fonts/Poppins-Black.ttf"),
+	});
+
+	if (!fontsLoaded) return null;
+
+	/* Render */
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen options={commonConfig} name="Setup" component={Setup} />
+				<Stack.Screen options={commonConfig} name="HowOften" component={HowOften} />
+				<Stack.Screen options={commonConfig} name="Camera" component={Camera} />
+				<Stack.Screen options={commonConfig} name="Preview" component={Preview} initialParams={{}} />
+				<Stack.Screen options={commonConfig} name="Result" component={Result} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default App;
