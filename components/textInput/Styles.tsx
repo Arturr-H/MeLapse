860ae6517539985550972e2757271c9c9f7b48cd
Artur <artur.h.0427@gmail.com>
2023-08-24 16:39:35 +0200

    Create `textInput` component

---
+++ components/textInput/Styles.tsx
@@ -0,0 +1,44 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+/* Constants */
+const WIDTH = Dimensions.get("window").width;
+const HEIGHT = Dimensions.get("window").height;
+
+export default StyleSheet.create({
+    textInputWrapper: {
+        width: "100%",
+        height: 50,
+        
+        backgroundColor: "#fff",
+        borderColor: "#eee",
+        borderWidth: 2,
+        borderRadius: 15,
+
+        paddingHorizontal: 20,
+
+        display: "flex",
+        flexDirection: "row"
+    },
+    textInput: {
+        flex: 2,
+        height: "100%",
+    },
+    focused: {
+        borderColor: "rgb(90, 200, 245)",
+        backgroundColor: "#f6f6f6"
+    },
+
+    charLenTextWrapper: {
+        height: "100%",
+        width: 50,
+        // backgroundColor: "red",
+
+        display: "flex",
+        justifyContent: "center",
+            alignItems: "center"
+    },
+    charLenText: {
+        color: "rgb(255, 69, 58)",
+        textAlign: "right",
+    }
+})
---
+++ components/textInput/TextInput.tsx
@@ -0,0 +1,108 @@
+import React, { RefObject } from "react";
+import { TextInput as RNTextInput, Animated, Dimensions, Easing, Image, TouchableOpacity, View, Text } from "react-native";
+import Styles from "./Styles";
+import { Animator } from "../animator/Animator";
+
+/* Interfaces */
+interface Props {
+    placeholder?: string,
+    onChangeText?: (text: string) => void,
+
+    minChars?: number,
+    maxChars?: number,
+    active: boolean
+}
+interface State {
+    focus: boolean,
+    chars: number,
+    value: string
+}
+
+export class TextInput extends React.PureComponent<Props, State> {
+    minMaxText: RefObject<Animator>;
+    min: number;
+    max: number;
+
+	constructor(props: Props) {
+		super(props);
+
+		/* State */
+		this.state = {
+            focus: false,
+            value: "",
+            chars: 0,
+        };
+
+        /* Static */
+        this.min = 1;
+        this.max = 16;
+
+        this.minMaxText = React.createRef();
+
+        this.tryGetValue = this.tryGetValue.bind(this);
+	}
+
+    /* Lifetime */
+    componentDidMount(): void {
+    }
+
+    /* On change the character length */
+    onCharLenChange(len: number): void {
+        this.setState({ chars: len });
+
+        if (len > this.max) {
+            this.minMaxText.current?.fadeIn().start();
+        }else if (len <= this.max) {
+            this.minMaxText.current?.fadeOut().start();
+        }
+    }
+
+    /* Try get the value of the input, if it's
+        invalid (more than max or less than)
+        min return null else return input value */
+    tryGetValue(): string | null {
+        const val = this.state.value;
+        const len = val.length;
+        
+        if (len < this.max && len >= this.min) {
+            return val;
+        }else if(len > this.max) {
+            this.minMaxText.current?.fadeIn().start();
+            return null;
+        }else {
+            this.minMaxText.current?.fadeIn().start();
+            return null;
+        }
+    }
+
+	render() {
+		return (
+            <View
+                style={[
+                    Styles.textInputWrapper,
+                    this.state.focus && Styles.focused
+                ]}
+            >
+                <RNTextInput
+                    style={Styles.textInput}
+                    placeholder={this.props.placeholder}
+                    onChangeText={(e) => {
+                        this.props.onChangeText && this.props.onChangeText(e);
+                        this.onCharLenChange(e.length);
+                        this.setState({ value: e });
+                    }}
+                    editable={this.props.active}
+
+                    onFocus={() => this.setState({ focus: true })}
+                    onBlur={() => this.setState({ focus: false })}
+                />
+
+                <View style={Styles.charLenTextWrapper}>
+                    <Animator startOpacity={0} ref={this.minMaxText}>
+                        <Text style={Styles.charLenText}>{this.state.chars}/{this.max}</Text>
+                    </Animator>
+                </View>
+            </View>
+        );
+	}
+}
