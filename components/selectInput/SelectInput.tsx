455892d3ca6dbc7f3b41f3cd28dc2c22e349bfb8
Artur <artur.h.0427@gmail.com>
2023-08-28 22:18:13 +0200

    Create component `selectInput`
    Can select either of multiple options

---
+++ components/selectInput/SelectInput.tsx
@@ -0,0 +1,99 @@
+/* Imports */
+import React from "react";
+import { Text, View } from "react-native";
+import Styles from "./Styles";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import * as Haptics from "expo-haptics";
+import Floater from "../floater/Floater";
+
+/* Interfaces */
+interface Props {
+    /** `idx`: the index of the button in
+     * the button array which was clicked */
+    onChange?: (idx: number) => void,
+
+    /** the buttons which will appear inside
+     * this component. 
+     * 
+     * buttons={["1", "2", "3"]}
+     */
+    buttons: string[]
+}
+interface State {
+    active: number
+}
+
+/** The input */
+class SelectInput extends React.PureComponent<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            active: 0
+        }
+    }
+
+    render(): React.ReactNode {
+        return (
+            <View style={Styles.selectInput}>
+                <Floater loosness={1} style={Styles.selectButtonWrapper}>
+                    {this.props.buttons.map((e, index) => 
+                        <SelectButton
+                            active={this.state.active === index}
+                            text={e}
+                            key={"selbtn-" + index}
+                            onPress={() => {
+                                this.setState({ active: index });
+                                Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                                this.props.onChange && this.props.onChange(index);
+                            }}
+                        />
+                    )}
+                </Floater>
+            </View>
+        )
+    }
+}
+
+/* Button */
+function SelectButton(props: {
+    text: string,
+    onPress: () => void,
+    active: boolean
+}) {
+    const [hover, setHover] = React.useState(false);
+
+    return (
+        <View style={{
+            flex: 1,
+        }}>
+            <TouchableHighlight
+                underlayColor={"rgb(80, 190, 245)"}
+                style={[
+                    Styles.selectButton,
+                    props.active && Styles.selectButtonActive,
+                    hover && Styles.selectButtonActive,
+                ]}
+                onPress={() => {
+                    props.onPress();
+                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+                }}
+                onPressIn={() => setHover(true)}
+                onPressOut={() => setHover(false)}
+            >
+                <Text style={[
+                    Styles.buttonText,
+                    props.active && Styles.buttonTextActive,
+                    hover && Styles.buttonTextActive,
+                ]}>{props.text}</Text>
+            </TouchableHighlight>
+        </View>
+    )
+}
+
+export default function(props: Props) {
+	const navigation = useNavigation();
+  
+	return <SelectInput {...props} />;
+}
---
+++ components/selectInput/Styles.tsx
@@ -0,0 +1,54 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+/* Constants */
+const WIDTH = Dimensions.get("window").width;
+const HEIGHT = Dimensions.get("window").height;
+
+export default StyleSheet.create({
+    selectInput: {
+        width: "100%",
+        height: 50,
+        
+        backgroundColor: "#eee",
+        borderColor: "#eee",
+        borderWidth: 2,
+        borderRadius: 15,
+
+        padding: 2,
+    },
+    selectButtonWrapper: {
+        width: "100%",
+        height: "100%",
+
+        flex: 1,
+        display: "flex",
+        flexDirection: "row",
+        gap: 5
+    },
+    selectButton: {
+        display: "flex",
+        justifyContent: "center",
+            alignItems: "center",
+
+        borderRadius: 10,
+
+        width: "100%",
+        height: "100%",
+
+    },
+    selectButtonActive: {
+        backgroundColor: "rgb(90, 200, 245)",
+        borderColor: "rgb(80, 190, 245)",
+        borderWidth: 2,
+    },
+    buttonText: {
+        color: "#000",
+        fontSize: 20,
+        fontFamily: "inter-black",
+    },
+    buttonTextActive: {
+        color: "#fff",
+        fontSize: 20,
+        fontFamily: "inter-black",
+    },
+})
