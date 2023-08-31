86eeecc9b5d40c1b8b7e7e475dc0aa512543617e
Artur <artur.h.0427@gmail.com>
2023-08-31 22:51:09 +0200

    Create `Config.tsx` ⚙
    Includes `AppConfig` class which has static only methods which work with `AsyncStorage` to save / retrieve configuration data

---
+++ scenes/preferences/Config.tsx
@@ -0,0 +1,41 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+/* Types */
+type StorageKey = "targetTimesPerDay" | "username" | "transformCamera";
+
+/* Enums */
+export enum TargetTimesPerDay { Once = 0, Twice = 1, Thrice = 2, NotSet = 3 }
+
+/**
+ * This is all the app's configuration which the user
+ * defines in the `Preferences.tsx` scene.
+ * 
+ * ### Usage
+ * ```js
+ * let config = await new AppConfig().tryLoad();
+ * ```
+*/
+export default class AppConfig {
+    private constructor() { throw new Error("AppConfig is not initializable. All methods are static") }
+
+    /** User name */
+    static async getUsername(): Promise<string> { return this.tryGet("username", "User") }
+    static async setUsername(value: string) { await AsyncStorage.setItem("username", JSON.stringify(value)) }
+
+    /** How many (target) times per day to take selfie */
+    static async getTargetTimesPerDay(): Promise<TargetTimesPerDay> { return await this.tryGet("targetTimesPerDay", TargetTimesPerDay.NotSet) as TargetTimesPerDay }
+    static async setTargetTimesPerDay(value: number) { await AsyncStorage.setItem("targetTimesPerDay", JSON.stringify(value)) }
+    
+    /** Transform camera in camera scene */
+    static async getTransformCamera(): Promise<boolean> { return this.tryGet("transformCamera", false) }
+    static async setTransformCamera(value: boolean) { await AsyncStorage.setItem("transformCamera", JSON.stringify(value)) }
+
+    private static async tryGet(key: StorageKey, default_: any): Promise<any> {
+        try {
+            /// "!" will never parse and will end up returning default_ in catch block
+            return JSON.parse(await AsyncStorage.getItem(key) ?? "!");
+        } catch {
+            return default_;
+        }
+    }
+}
