5d0846509b4d1a642fe775e5971f1adebbd14005
Artur <artur.h.0427@gmail.com>
2023-09-09 13:46:08 +0200

    Add methods to `Image` class
    Add static `fromLSImageProp`, `getImagePointers` methods and non-static `toLSImageProp` ⚡

--- functional/Image.tsx
+++ functional/Image.tsx
@@ -9,16 +9,29 @@ import { formatDate } from "./Date";
 /// as jpg in the end (it's good for space)
 const EXT = "JPG";
 
-/* Locally Stored Image */
-export class LSImage {
-	/// Path to the file which is stored in fs
+/**
+ * The `LSImage` class can't be passed as prop, it leads 
+ * to the warning "Non-serializable values were found in
+ * the navigation state". Therefore we have this interface
+ * which only has the neccesary values (not including methods)
+ * for converting it back into a LSImage.
+*/
+export interface LSImageProp {
+	/** Path to the file which is stored in fs */
 	path: string;
 
-	/// Used when storing images (is randomly generated)
-	/// and can be used for ex keys.
-	private filename: string;
+	/**	Used when storing images (is randomly
+	 * generated) and can be used for ex keys. */
+	filename: string;
 	
-	/// Unix time stamp (ms)
+	/** Unix time stamp (ms) */
+	date: number;
+}
+
+/** Locally Stored Image */
+export class LSImage {
+	path: string;
+	private filename: string;
 	date: number;
 
 	constructor(path: string) {
@@ -46,11 +59,38 @@ export class LSImage {
 		saveImage(this, callback);
 	}
 
+	/** Convert `LSImageProp` into this class */
+	static fromLSImageProp(prop: LSImageProp): LSImage {
+		let lsimage = new LSImage(prop.path);
+		lsimage.filename = prop.filename;
+		lsimage.date = prop.date;
+
+		return lsimage;
+	}
+	/** Convert this class into `LSImageProp` */
+	toLSImageProp(): LSImageProp {
+		return {
+			path: this.path,
+			filename: this.filename,
+			date: this.date
+		}
+	}
+
 	/* Getters */
 	getDateFormatted(): string { return formatDate(this.date) };
 	getFilename(): string { return this.filename };
 	getDate(): number { return this.date };
 	getPath(): string { return this.path };
+
+	/** Gets all image paths */
+	public static async getImagePointers(): Promise<LSImageProp[] | null> {
+		try {
+			const ret = await AsyncStorage.getItem("imagePointers");
+			return JSON.parse(ret!) as LSImageProp[];
+		} catch {
+			return null
+		}
+	}
 }
 
 /* Save image to users fs */
