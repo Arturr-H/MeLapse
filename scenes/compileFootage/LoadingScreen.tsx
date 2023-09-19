bb145bbd8110f4dbd78d547c426288f4c71cfc40
Artur <artur.h.0427@gmail.com>
2023-09-19 16:54:08 +0200

    Display correct stats in `LoadingScreen` scene

--- scenes/compileFootage/LoadingScreen.tsx
+++ scenes/compileFootage/LoadingScreen.tsx
@@ -6,38 +6,57 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { stitchImages } from "../../functional/VideoCreator";
 import * as MediaLibrary from "expo-media-library";
-import { WIDTH } from "../result/Result";
+import { WIDTH, getTimeAgo } from "../result/Result";
 import QuickCounter from "./QuickCounter";
 import MultiAnimator from "../../components/animator/MultiAnimator";
 import { FFmpegKit } from "ffmpeg-kit-react-native";
+import { Button } from "../../components/button/Button";
+import Poster from "../../components/poster/Poster";
+import { LSImage, LSImageProp } from "../../functional/Image";
+import { StatusBar } from "expo-status-bar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { formatDate } from "../../functional/Date";
 
 
 /* Interfaces */
+interface Stats {
+    daysSinceFirstSelfie: number,
+    selfiesCaptures: number,
+    mostImagesOneDay: DateAndNr,
+    numberOfScrappedSelfies: number,
+    firstImage?: LSImageProp,
+}
 interface Props {
-    navigation: StackNavigationProp<{ Composer: undefined }, "Composer">,
-    
-    params: LoadingScreenRouteParams
+    navigation: StackNavigationProp<{ Composer: undefined, Camera: { comesFrom: "other" } }, "Composer" | "Camera">,
+
+    params: StitchOptions
 }
 interface State {
     amountOfImages: number,
     amountOfImagesTranslateX: Animated.Value,
-    
+
     /** This scene displays a bunch of stats, this
      * value keeps track of which to display */
-    statIndex: number
+    statIndex: number,
+
+    loading: boolean,
+
+    /** Stats when loading */
+    stats: Stats
 }
-export interface LoadingScreenRouteParams {
+export interface StitchOptions {
     fps: number,
     outputFormat: "gif" | "mp4",
-    quality: "okay" | "mid" | "high"
+    quality: "okay" | "mid" | "high",
+    bitrateOverride: number | null,
+
+    widthOverride: number | null,
 }
 
 class LoadingScreen extends React.Component<Props, State> {
     /* Refs */
     multianimator: RefObject<MultiAnimator> = React.createRef();
-
-    /* Other */
-    statComponents: JSX.Element[];
+    posterHasAnimated: boolean = false;
 
     constructor(props: Props) {
         super(props);
@@ -47,36 +66,33 @@ class LoadingScreen extends React.Component<Props, State> {
             amountOfImages: 0,
             amountOfImagesTranslateX: new Animated.Value(-WIDTH),
 
-            statIndex: 0
+            statIndex: 0,
+            loading: true,
+
+            stats: {
+                daysSinceFirstSelfie: 0,
+                selfiesCaptures: 0,
+                mostImagesOneDay: { date: 0, nr: 0 },
+                numberOfScrappedSelfies: 0,
+            }
         };
-
-        /* The stats */
-        this.statComponents = [
-            <MultiAnimator ref={this.multianimator}>
-                <QuickCounter from={0} to={188} duration={4000} callback={this.nextStat} />
-                <Text style={Styles.amountOfImagesText}>Selfies captured</Text>
-            </MultiAnimator>,
-
-            <MultiAnimator>
-                <QuickCounter from={0} to={188} duration={4000} callback={this.nextStat} />
-                <Text style={Styles.amountOfImagesText}>Shits made</Text>
-            </MultiAnimator>,
-        ];
 
         /* Bindings */
         this.generateVideo = this.generateVideo.bind(this);
         this.nextStat = this.nextStat.bind(this);
         this.goBack = this.goBack.bind(this);
     };
-    
+
     /* Lifetime */
     async componentDidMount(): Promise<void> {
-        console.log(this.props.params);
-        // this.bigText.current.
         await this.generateVideo();
+
         this.setState({ amountOfImages: 10 });
         this.animateAmountOfImages();
         this.multianimator.current?.fadeIn(200, 100, undefined, 1000);
+
+        const imagePointers = await LSImage.getImagePointers();
+        if (imagePointers) this.setState({ stats: await getAllStats(imagePointers) });
     }
 
     /** Animate intro for amount of images text */
@@ -93,9 +109,11 @@ class LoadingScreen extends React.Component<Props, State> {
     async generateVideo(): Promise<void> {
         try {
             await stitchImages(async (e) => {
-                await MediaLibrary.saveToLibraryAsync(e);
-            });
-        }catch {
+                await MediaLibrary.saveToLibraryAsync(e).then(() => {
+                    this.setState({ loading: false });
+                });
+            }, this.props.params);
+        } catch {
             alert("Couldn't generate video");
         }
     }
@@ -109,36 +127,63 @@ class LoadingScreen extends React.Component<Props, State> {
         }, 400)
     }
 
-    /** Cancels the ffmpeg gif stitching process
+    /** Can cancel the ffmpeg gif stitching process
      * and switches back scene */
-    goBack(): void {
+    goBack(shouldCancel: boolean, to: "Camera" | "Composer"): void {
+        let nav: () => void;
+        if (to === "Camera") {
+            nav = () => this.props.navigation.navigate(to, { comesFrom: "other" });
+        } else {
+            nav = () => this.props.navigation.navigate(to);
+        };
+
         this.multianimator.current?.fadeOut(200, 100, () => {
-            FFmpegKit.cancel().then(() => {
-                this.props.navigation.navigate("Composer");
-            });
+            if (shouldCancel === true) {
+                FFmpegKit.cancel().then(nav);
+            } else {
+                nav()
+            }
         });
     }
 
-	render() {
+    render() {
         const statComponents: (JSX.Element[])[] = [
             [
-                <QuickCounter from={0} to={188} duration={4000} callback={this.nextStat} />,
+                <Poster lsimage={this.state.stats.firstImage} ref={e => {
+                    if (this.posterHasAnimated === false) {
+                        this.posterHasAnimated = true;
+                        setTimeout(() => {
+                            this.nextStat();
+                            e?.save(() => { });
+                        }, 5000)
+                    };
+                }} />,
+                <Text style={[Styles.amountOfImagesText, { transform: [{ translateY: -100 }], textAlign: "center" }]}>😉 Your first selfie</Text>
+            ],
+
+            [
+                <QuickCounter from={0} to={Math.round((new Date().getTime() - this.state.stats.daysSinceFirstSelfie) / (24 * 60 * 60 * 1000))} duration={4000} callback={this.nextStat} />,
+                <Text style={Styles.amountOfImagesText}>⏱️ Days since first selfie ({formatDate(this.state.stats.daysSinceFirstSelfie)})</Text>
+            ],
+
+            [
+                <QuickCounter from={0} to={this.state.stats.selfiesCaptures} duration={4000} callback={this.nextStat} />,
                 <Text style={Styles.amountOfImagesText}>📸 Selfies captured</Text>
             ],
 
             [
-                <QuickCounter from={0} to={431} duration={4000} callback={this.nextStat} />,
-                <Text style={Styles.amountOfImagesText}>⏱️ Days since first selfie</Text>
+                <QuickCounter from={0} to={this.state.stats.mostImagesOneDay.nr} duration={2000} delay={2000} callback={this.nextStat} />,
+                <Text style={Styles.amountOfImagesText}>🔥 Most images taken on one day ({formatDate(this.state.stats.mostImagesOneDay.date)})</Text>
             ],
 
             [
-                <QuickCounter from={0} to={4} duration={2000} delay={2000} callback={this.nextStat} />,
-                <Text style={Styles.amountOfImagesText}>🔥 Most images taken on one day</Text>
+                <QuickCounter from={0} to={this.state.stats.numberOfScrappedSelfies} duration={4000} callback={this.nextStat} />,
+                <Text style={Styles.amountOfImagesText}>🗑️ Total amount of images scrapped</Text>
             ],
 
             [
                 <Text style={Styles.thankYou}>Thank you for using {"my app"}</Text>,
-                <Text style={Styles.thankYou}>❤️</Text>
+                <Text style={Styles.thankYou}>😄</Text>
             ],
         ];
 
@@ -147,25 +192,14 @@ class LoadingScreen extends React.Component<Props, State> {
             if (index === this.state.statIndex) { return e as JSX.Element[] }
             else { return [] as JSX.Element[] }
         });
-        
-		return (
-			<SafeAreaView style={Styles.container}>
-                <TouchableOpacity style={Styles.cancelButton} activeOpacity={0.5} onPress={this.goBack}>
+
+        return (
+            <SafeAreaView style={Styles.container}>
+                <TouchableOpacity style={Styles.cancelButton} activeOpacity={0.5} onPress={() => this.goBack(true, "Composer")}>
                     <Text style={Styles.cancelButtonText}>← Cancel</Text>
                 </TouchableOpacity>
 
                 <View style={Styles.padding}>
-                    {/* <View style={Styles.emojiView}>
-                        <View style={Styles.emojiCorner}>
-                            <Text style={Styles.emojiCornerTextTop}>📸</Text>
-                        </View>
-                        <View style={Styles.emojiCorner}>
-                            <Text style={Styles.emojiCornerTextBottom}>📸</Text>
-                        </View>
-
-                        <BlurView intensity={40} style={Styles.emojiViewBlur} />
-                    </View> */}
-
                     <View style={Styles.innerContainer}>
                         <MultiAnimator ref={this.multianimator}>
                             {children}
@@ -173,17 +207,88 @@ class LoadingScreen extends React.Component<Props, State> {
                     </View>
 
                     <View style={Styles.loadingContainer}>
-                        <Text style={Styles.loadingText}>Generating</Text>
-                        <ActivityIndicator color={"#ddd"} />
+                        {this.state.loading ? (
+                            <React.Fragment>
+                                <Text style={Styles.loadingText}>Generating</Text>
+                                <ActivityIndicator color={"#ddd"} />
+                            </React.Fragment>
+                        )
+                            : (
+                                <View style={{ flexDirection: "column", gap: 10, width: "100%" }}>
+                                    <Text style={Styles.loadingText}>Done Generating 🎉</Text>
+                                    <Button flex active={true} color="blue" onPress={() => this.goBack(false, "Camera")} text="Okay" />
+                                </View>
+                            )}
                     </View>
                 </View>
-			</SafeAreaView>
-		);
-	}
+
+                <StatusBar style="dark" />
+            </SafeAreaView>
+        );
+    }
 }
 
-export default function(props: any) {
-	const navigation = useNavigation();
+export default function (props: any) {
+    const navigation = useNavigation();
 
     return <LoadingScreen params={props.route.params} {...props} navigation={navigation} />;
+}
+
+
+async function getAllStats(lsimages: LSImageProp[]): Promise<Stats> {
+    let firstImage_: LSImageProp | null = null;
+    for (let i = 0; i < lsimages.length; i++) {
+        const oldestDate = (firstImage_ ?? { date: Number.MAX_SAFE_INTEGER }).date;
+        if (lsimages[i].date < oldestDate) firstImage_ = lsimages[i];
+    }
+
+    const mostImagesOneDay = getMostOnOneDay(lsimages.map(e => e.date));
+    const daysSinceFirstSelfie = firstImage_?.date ?? 0;
+    const firstImage = firstImage_ ?? undefined;
+    const selfiesCaptures = lsimages.length;
+    let numberOfScrappedSelfies: number;
+    try {
+        const c = await AsyncStorage.getItem("scrappedSelfies") ?? "!";
+        numberOfScrappedSelfies = JSON.parse(c);
+    }catch {
+        numberOfScrappedSelfies = 0;
+    }
+
+    return { firstImage, mostImagesOneDay, daysSinceFirstSelfie, numberOfScrappedSelfies, selfiesCaptures }
+}
+
+/** Get the most amount of pictures were taken on a day */
+type DateAndNr = { nr: number, date: number };
+function getMostOnOneDay(unixTimestamps: number[]): DateAndNr {
+    const dayCounts: { [day: string]: DateAndNr } = {};
+
+    // Iterate through the timestamps and count the occurrences of each day
+    unixTimestamps.forEach((timestamp) => {
+        const date = new Date(timestamp);
+        const dayKey = date.toDateString(); // Extract the day and use it as the key
+
+        if (dayCounts[dayKey]) {
+            dayCounts[dayKey].nr++;
+        } else {
+            dayCounts[dayKey] = { date: timestamp, nr: 1 };
+        }
+    });
+
+    // Find the day(s) with the maximum count
+    let maxCount = 0;
+    let mostRecordedDay: DateAndNr = { nr: 0, date: 0 };
+
+    for (const dayKey in dayCounts) {
+        if (dayCounts.hasOwnProperty(dayKey)) {
+            const count = dayCounts[dayKey];
+            if (count.nr > maxCount) {
+                maxCount = count.nr;
+                mostRecordedDay = count;
+            } else if (count.nr === maxCount) {
+                mostRecordedDay = count;
+            }
+        }
+    };
+
+    return mostRecordedDay
 }
