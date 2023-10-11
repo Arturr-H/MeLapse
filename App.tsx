2c1064da6cf3dcb46f99caf7eefc0208b89a3960
Artur <artur.h.0427@gmail.com>
2023-10-11 16:40:28 +0200

    Implement new notification setup and include some scenes

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 /* Resources */
 import { useFonts } from "expo-font";
 import { Asset } from "expo-asset";
-import { Image } from "react-native";
+import { Image, Platform } from "react-native";
 import * as ExpoSplashScreen from "expo-splash-screen";
 
 /* Scene imports */
@@ -14,7 +14,6 @@ import Preview from "./scenes/preview/Preview";
 import Result from "./scenes/result/Result";
 
 import { enableScreens } from "react-native-screens";
-import Setup from "./scenes/setup/Name";
 import HowOften from "./scenes/setup/HowOften";
 import Preferences from "./scenes/preferences/Preferences";
 import Calibration from "./scenes/calibration/Calibration";
@@ -25,7 +24,23 @@ import Review from "./scenes/review/Review";
 import AdvancedComposer from "./scenes/composer/AdvancedComposer";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Tutorial from "./scenes/tutorial/Tutorial";
+import PrivacyPolicy from "./scenes/privacyPolicy/PrivacyPolicy";
+import Statistics from "./scenes/statistics/Statistics";
+import Welcome from "./scenes/setup/Welcome";
 enableScreens();
+
+/* Notifications */
+import * as Device from "expo-device"
+import Constants from "expo-constants";
+import * as Notifications from "expo-notifications";
+import { allowsNotificationsAsync, useLocalNotification } from "./LocalNotification";
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false,
+	}),
+});
 
 ExpoSplashScreen.preventAutoHideAsync();
 
@@ -33,38 +48,41 @@ ExpoSplashScreen.preventAutoHideAsync();
 const Stack = createStackNavigator();
 
 const App = () => {
-	const [initialRoute, setInitialRoute] = React.useState<"Camera" | "Setup">("Setup");
-
+	useLocalNotification();
+	const [initialRoute, setInitialRoute] = React.useState<"Camera" | "Welcome">("Welcome");
+	
 	/* Config for scenes (avoiding repetition) */
 	let commonConfig = { headerShown: false, animationEnabled: false };
-
+	
 	/* Hooks */
 	let [resourcesLoaded, setResourcesLoaded] = React.useState(false);
-
+	
 	/* Component did mount hehe */
 	React.useEffect(() => {
-		async function loadResourcesAsync() {
+		async function loadAsync() {
+			/* Remove later? */
 			try {
 				const imageAssets = cacheImages([
 					require("./assets/images/paper-thing.png"),
 					require("./assets/images/pin.png"),
 				]);
-				
+
 				await Promise.all(imageAssets);
 			} catch (e) {
 				console.warn(e);
 			} finally {
 				setResourcesLoaded(true);
 				ExpoSplashScreen.hideAsync();
-			}
+			};
 		};
 
+		/* First screen */
 		AsyncStorage.getItem("setupComplete").then((value) => {
 			if (value === "true")
 			  	setInitialRoute("Camera");
 		});
 
-		loadResourcesAsync();
+		loadAsync();
 	}, []);
 
 	/* Fonts */
@@ -82,11 +100,12 @@ const App = () => {
 		<NavigationContainer>
 			<Stack.Navigator initialRouteName={initialRoute}>
 				<Stack.Screen options={commonConfig} name="Camera" component={Camera} initialParams={{ comesFrom: "other" }} />
-				<Stack.Screen options={commonConfig} name="Preview" component={Preview} initialParams={{}} />
+				<Stack.Screen options={commonConfig} name="Preview" component={Preview} />
 				<Stack.Screen options={commonConfig} name="Result" component={Result} />
 
 				{/* Preferences */}
 				<Stack.Screen options={commonConfig} name="Preferences" component={Preferences} />
+				<Stack.Screen options={commonConfig} name="Statistics" component={Statistics} />
 				<Stack.Screen options={commonConfig} name="Review" component={Review} />
 
 				{/* Composer */}
@@ -95,10 +114,13 @@ const App = () => {
 				<Stack.Screen options={commonConfig} name="LoadingScreen" component={LoadingScreen} />
 
 				{/* Setup */}
-				<Stack.Screen options={commonConfig} name="Setup" component={Setup} />
-				<Stack.Screen options={commonConfig} name="HowOften" component={HowOften} />
+				<Stack.Screen options={commonConfig} name="Welcome" component={Welcome} />
+				<Stack.Screen options={commonConfig} name="HowOften" component={HowOften} initialParams={{ confirmLocation: "Calibration" }} />
 				<Stack.Screen options={commonConfig} name="Calibration" component={Calibration} />
 				<Stack.Screen options={commonConfig} name="Tutorial" component={Tutorial} />
+
+				{/* Privacy policy */}
+				<Stack.Screen options={commonConfig} name="PrivacyPolicy" component={PrivacyPolicy} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
