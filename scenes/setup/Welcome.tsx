a07aa909df61ab2fa8d89fd66232d8215890c9bd
Artur <artur.h.0427@gmail.com>
2023-10-13 18:57:25 +0200

    Add privacy policy modal in `welcome` scene

--- scenes/setup/Welcome.tsx
+++ scenes/setup/Welcome.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { RefObject } from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import Styles from "./Styles";
 import { Button } from "../../components/button/Button";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import { ModalConstructor } from "../../components/modal/ModalConstructor";
 
 /* Interfaces */
 export interface Props {
-    navigation: StackNavigationProp<{ HowOften: { confirmLocation: "Calibration" } }, "HowOften">,
+    navigation: StackNavigationProp<{
+        HowOften: { confirmLocation: "Calibration" },
+        PrivacyPolicy: { confirmLocation: "Welcome" },
+    }, "HowOften" | "PrivacyPolicy">,
 }
 
 export class Welcome extends React.PureComponent<Props, {}> {
+    modalConstructor: RefObject<ModalConstructor> = React.createRef();
+
     constructor(props: Props) {
         super(props);
 
@@ -20,11 +26,24 @@ export class Welcome extends React.PureComponent<Props, {}> {
     };
 
     async onConfirm(): Promise<void> {
-        this.props.navigation.navigate("HowOften", { confirmLocation: "Calibration" });
+        const read = () => this.props.navigation.navigate("PrivacyPolicy", { confirmLocation: "Welcome" });
+        const confirm = () => this.props.navigation.navigate("HowOften", { confirmLocation: "Calibration" });
+
+        this.modalConstructor.current?.constructModal({
+            header: "Privacy Policy",
+            description: "Would you like to read the privacy policy? (Can be read later on)",
+
+            buttons: [
+                { text: "Read", onClick: read, color: "blue", hollow: true },
+                { text: "Accept", onClick: confirm, color: "blue" },
+            ]
+        })
     }
 
     render() {
         return (
+            <React.Fragment>
+            <ModalConstructor ref={this.modalConstructor} />
             <SafeAreaView style={Styles.container}>
                 <View style={Styles.column}>
                     <View style={Styles.body}>
@@ -46,6 +65,7 @@ export class Welcome extends React.PureComponent<Props, {}> {
 
                 <StatusBar style="dark" />
             </SafeAreaView>
+            </React.Fragment>
         );
     }
 }
