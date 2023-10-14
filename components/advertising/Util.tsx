6c7a88d01a8fd6b4af59dcce08d2967ee6a7a51f
Artur <artur.h.0427@gmail.com>
2023-10-14 12:41:59 +0200

    Remove `Util.tsx` file from `advertising` component

--- components/advertising/Util.tsx
+++
@@ -1,8 +0,0 @@
-/* @ts-ignore */
-import { BANNER, REWARDED } from "@env";
-import { TestIds } from "react-native-google-mobile-ads";
-
-export const IS_PRODUCTION = process.env.EXPO_PUBLIC_PRODUCTION_ADS;
-
-export const BANNER_UNIT_ID = IS_PRODUCTION === "true" ? BANNER : TestIds.BANNER;
-export const REWARDED_UNIT_ID = IS_PRODUCTION === "true" ? REWARDED : TestIds.REWARDED;
