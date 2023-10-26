d5f36e04d4b2b033896a50ff9ddca1467b84e86f
Artur <artur.h.0427@gmail.com>
2023-10-26 15:08:45 +0200

    Add `.env` variable `DEV_ENV`

--- env.pubilc.ts
+++ env.pubilc.ts
@@ -1,3 +1,7 @@
 export const env = {
-    PRODUCTION_ADS: true
+    PRODUCTION_ADS: true,
+
+    /** Enable dev features such as turning off
+     * calibration checks on simulators and more */
+    DEV_ENV: true
 }
