1299888f6be9b2775aee1ae1b27da99fd591c257
Artur <artur.h.0427@gmail.com>
2023-10-28 23:06:11 +0200

    Add `text` component for quickly creating headers etc

---
+++ components/text/Styles.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+import MenuBundle from "../../styleBundles/MenuBundle";
+import { Colors } from "../../styleBundles/Colors";
+
+export default StyleSheet.create({
+    ...MenuBundle,
+
+    titleContainer: {
+        display: "flex",
+        flexDirection: "row",
+
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    questionMarkButton: {
+        fontSize: 20,
+        fontFamily: "manrope-black",
+        color: Colors.text.light,
+        opacity: 0.7
+    },
+    headerCentered: {
+        textAlign: "center",
+
+        fontFamily: "manrope-black",
+        fontSize: 24,
+    },
+
+    h2: {
+        fontFamily: "manrope-black",
+        fontSize: 24,
+        marginBottom: 5,
+
+        color: "#000"
+    },
+    h3: {
+        fontFamily: "manrope-black",
+        fontSize: 16,
+        marginBottom: 5,
+
+        color: Colors.text.light
+    },
+})
---
+++ components/text/Title.tsx
@@ -0,0 +1,31 @@
+import { Alert, Text, TouchableOpacity, View } from "react-native"
+import Styles from "./Styles"
+
+
+export const TitleH3 = (props: { title: string, info?: string }) => {
+    return <View style={Styles.titleContainer}>
+        <Text style={Styles.h3}>{props.title}</Text>
+
+        {props.info && <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => Alert.alert(
+                "Info 🤔",
+                props.info,
+                [{ text: "Okay" }]
+            )}
+        >
+            <Text style={Styles.questionMarkButton}>?</Text>
+        </TouchableOpacity>}
+    </View>
+}
+export const TitleH2 = (props: { title: string }) => {
+    return <Text style={Styles.h2}>{props.title}</Text>
+}
+export const TitleH1 = (props: { title: string }) => {
+    return <Text style={Styles.header}>{props.title}</Text>
+}
+
+
+export const TitleCentered = (props: { title: string }) => {
+    return <Text style={Styles.headerCentered}>{props.title}</Text>
+}
